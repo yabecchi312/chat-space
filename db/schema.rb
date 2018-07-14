f5b671651c23704eee92f3b5aad9a8f6dc59b0ce
yabecchi312 <yabehideaki@MacBook-Pro-4.local>
2018-07-14 13:35:44 +0900

    destroy model
    modelの削除

--- db/schema.rb
+++ db/schema.rb
@@ -10,44 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180713091755) do
+ActiveRecord::Schema.define(version: 0) do
 
-  create_table "groups", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
-    t.string   "name",       null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["name"], name: "index_groups_on_name", using: :btree
-  end
-
-  create_table "members", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
-    t.integer  "user_id"
-    t.integer  "group_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["group_id"], name: "index_members_on_group_id", using: :btree
-    t.index ["user_id"], name: "index_members_on_user_id", using: :btree
-  end
-
-  create_table "messages", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
-    t.text     "body",       limit: 65535, null: false
-    t.string   "image",                    null: false
-    t.integer  "group_id",                 null: false
-    t.integer  "user_id",                  null: false
-    t.datetime "created_at",               null: false
-    t.datetime "updated_at",               null: false
-    t.index ["group_id"], name: "index_messages_on_group_id", using: :btree
-    t.index ["user_id"], name: "index_messages_on_user_id", using: :btree
-  end
-
-  create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
-    t.string   "name",       null: false
-    t.string   "email",      null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  add_foreign_key "members", "groups"
-  add_foreign_key "members", "users"
-  add_foreign_key "messages", "groups"
-  add_foreign_key "messages", "users"
 end
