2a2801a9cab713af2274a597e7410de9e94478c1
yabecchi312 <yabehideaki@MacBook-Pro-4.local>
2018-07-19 17:30:20 +0900

    message model create

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,5 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :trackable, :validatable
   has_many :group_users
   has_many :groups, through: :group_users
+  has_many :messages
 end
