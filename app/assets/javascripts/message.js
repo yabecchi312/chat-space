bcbc10fd6c94c5805ebd9a9c8c97cbf938f29eea
yabecchi312 <yabehideaki@MacBook-Pro-4.local>
2018-07-23 13:41:32 +0900

    add ajax
    イベントが発火したときにAjaxを使用して、messages#createが動くようにする

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,7 +1,14 @@
 $(function(){
   $('#new_message').on('submit', function(e){
     e.preventDefault();
-    console.log(this)
     var formData = new FormData(this);
+    var url = $(this).attr('action')
+    $.ajax({
+      url: url,
+      type: "POST",
+      data: formData,
+      dataType: 'json',
+      processData: false,
+      contentType: false
   })
 })
