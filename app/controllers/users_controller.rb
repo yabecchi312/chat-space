7278bb3e36a80aaffa805d6688e61c99c0dc5c94
yabecchi312 <yabehideaki@MacBook-Pro-4.local>
2018-07-24 11:38:28 +0900

    add index action respond_to
    ルーティングなどAPI側の準備をする。
    view/user/index.json.jbuilderを作成。

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,10 @@
 class UsersController < ApplicationController
   def index
+    @users = User.where('name LIKE(?)', "%#{params[:keyword]}%").where.not(id: current_user.id)
+    respond_to do |format|
+      format.html
+      format.json
+    end
   end
 
   def edit
